a5f2f2f95d0cd8703b429254171cfe46994c14f1
ginpei <ginpei@gmail.com>
2017-06-16 00:45:47 -0700

    jQuery

--- index.js
+++ index.js
@@ -1,47 +1,4 @@
 (function() {
-	function $(selector, parent=document) {
-		const els = Array.from(parent.querySelectorAll(selector));
-		return {
-			each(fn) {
-				els.forEach(fn);
-				return this;
-			},
-
-			on(type, listener) {
-				this.each((el)=>el.addEventListener(type, listener));
-				return this;
-			},
-
-			addClass(namesString) {
-				const names = namesString.split(' ');
-				names.forEach((name)=>{
-					this.each(el=>el.classList.add(name));
-				});
-				return this;
-			},
-
-			text(text) {
-				if (arguments.length > 0) {
-					this.each(el=>el.textContent=text);
-					return this;
-				}
-				else {
-					return els[0].textContent;
-				}
-			},
-
-			val(value) {
-				if (arguments.length > 0) {
-					this.each(el=>el.value=value);
-					return this;
-				}
-				else {
-					return els[0].value;
-				}
-			},
-		};
-	}
-
 	$('.js-searchForm').on('submit', (event)=>{
 		event.preventDefault();
 		$('body').addClass('is-showingResult');
