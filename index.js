ead47aaee380af262a0fd6d22bbfdc9e09cd2771
ginpei <ginpei@gmail.com>
2017-06-16 00:54:31 -0700

    show result

--- index.js
+++ index.js
@@ -4,5 +4,11 @@
 		$('body').addClass('is-showingResult');
 		$('.js-keyword').text($('.js-keyword').val());
 		$('.js-loading').prop('hidden', false);
+		$('.js-resultList').prop('hidden', true);
+
+		setTimeout(()=>{
+			$('.js-loading').prop('hidden', true);
+			$('.js-resultList').prop('hidden', false);
+		}, 3500);
 	});
 })();
